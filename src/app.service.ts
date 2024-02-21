553ec71ee027135e5daee77cfe078a6786e42f00
GautamAgnihotri15 <goutamagnihotri04@gmail.com>
2024-02-21 19:48:56 +0530

    add demo controllers.

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,20 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-  getsomething(): string {
-    return "It is something method"
+  getAllUser(): string {
+    return "Get data of all users"
   }
+  getUser(): string {
+    return "Get User"
+  }
+  addUser(): string {
+    return "User added"
+  }
+  updateUserInfo(): string{
+    return "User detail updated"
+  }
+  deleteUser(): string {
+    return "user deleted"
+  }
+
 }
